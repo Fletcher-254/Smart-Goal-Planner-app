25702cf5de567c06b6bef26088d4a377625316f6
fletcher-254 <fletcher.nyawira@student.moringaschool.com>
2025-07-21 04:37:57 +0300

    Add navigation links to Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,42 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white p-4">
-      <nav className="flex justify-between items-center">
-        <h1 className="text-xl font-bold">Smart Goal Planner</h1>
-        <ul className="flex space-x-4">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+    <header className="bg-blue-600 text-white p-4 shadow">
+      <nav className="flex justify-between items-center max-w-5xl mx-auto">
+        <h1 className="text-xl font-bold">
+          <Link to="/">Smart Goals</Link>
+        </h1>
+        <ul className="flex space-x-6">
+          <li>
+            <Link to="/" className="hover:underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/goals" className="hover:underline">
+              Goals
+            </Link>
+          </li>
+          <li>
+            <Link to="/create-goal" className="hover:underline">
+              Create Goal
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="hover:underline">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register" className="hover:underline">
+              Register
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
