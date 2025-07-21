a10c5445101fb572b926da0d8ef8d0e820800873
fletcher-254 <fletcher.nyawira@student.moringaschool.com>
2025-07-21 14:36:52 +0300

    correcting error in Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="text-center text-sm py-4 text-gray-500 border-t mt-10">
-      © {new Date().getFullYear()} SmartGoal Planner. Built with 💙 by You.
+      © {new Date().getFullYear()} SmartGoal Planner.
     </footer>
   );
 }
